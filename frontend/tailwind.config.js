32e4ddf34e6dd243d0cada0ff5a176ebaa847bba
Rodrigo Federico Isidro Paz <149693028+Rodrigopz52@users.noreply.github.com>
2026-06-08 22:17:10 -0300

    style(theme): actualizar paleta de color principal a morado #912EC7

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          50: '#fdf4ff',
+          100: '#fae6ff',
+          200: '#f3ccff',
+          300: '#e7a3fe',
+          400: '#d36af8',
+          500: '#be3eec',
+          600: '#912EC7', // Nuevo color base solicitado
+          700: '#8121af',
+          800: '#6b1d91',
+          900: '#571b74',
+        }
+      }
+    },
   },
   plugins: [],
 }
